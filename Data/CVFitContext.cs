072f460d2831e37127fb8002d2032d5a04b73a19
Chris-Costa <cdcosta807@gmail.com>
2022-10-27 15:49:25 -0400

    seeding the db and creating new migartion.  installed extensionsqlite compact toolbox

--- Data/CVFitContext.cs
+++ Data/CVFitContext.cs
@@ -18,6 +18,104 @@ namespace project_backend.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().HasData(
+                new Blog()
+                { 
+                    Id=1,
+                    Title="Test1",
+                    Author="Alexander",
+                    Content="Content for test1",
+                    Avatar="x",
+                    Likes=5
+                },
+                new Blog()
+                {
+                    Id=2,
+                    Title="Test2",
+                    Author="Alonso",
+                    Content="Content for test2",
+                    Avatar="x",
+                    Likes=24
+                },
+                new Blog()
+                {
+                    Id=3,
+                    Title="Test3",
+                    Author="Anand",
+                    Content="Content for test3",
+                    Avatar="x",
+                    Likes=12
+                });
+
+            modelBuilder.Entity<Exercise>().HasData(
+                new Exercise()
+                {
+                    Id=1,
+                    Name="Pushups",
+                    MuscleGroup="Chest",
+                    Description="Some Descr",
+                    RepRange="10-20",
+                    Difficulty="Easy",
+                    ImageURL="x",
+                    VideoURL="x"
+                },
+                new Exercise()
+                {
+                    Id=2,
+                    Name="Bench",
+                    MuscleGroup="Chest",
+                    Description="Some Descr",
+                    RepRange="5-10",
+                    Difficulty="Medium",
+                    ImageURL="x",
+                    VideoURL="x"
+                },
+                new Exercise()
+                {
+                    Id=3,
+                    Name="Fly's",
+                    MuscleGroup="Chest",
+                    Description="Some Descr",
+                    RepRange="10-15",
+                    Difficulty="Easy",
+                    ImageURL="x",
+                    VideoURL="x"
+                });
+            
+            modelBuilder.Entity<User>().HasData(
+                new User()
+                { 
+                    Id=1,
+                    Firstname="Linda",
+                    Lastname="Martin",
+                    Username="LMartin567",
+                    Avatar="x",
+                    WeightGoal=145
+                },
+                new User()
+                {
+                    Id=2,
+                    Firstname="Jack",
+                    Lastname="Smith",
+                    Username="JSmith215",
+                    Avatar="x",
+                    WeightGoal=175
+                },
+                new User()
+                {
+                    Id=3,
+                    Firstname="Cindy",
+                    Lastname="Long",
+                    Username="CLong498",
+                    Avatar="x",
+                    WeightGoal=115
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
         /*
         public CVFitContext()
         {
--- Entities/Blog.cs
+++ Entities/Blog.cs
@@ -10,16 +10,11 @@ namespace project_backend.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  //generate new key when blog post is added
         public int Id { get; set; }
         public string? Title { get; set; }
-        public string Author { get; set; }
+        public string? Author { get; set; }
         public string? Content { get; set; }
         public string? Avatar { get; set; }
         public int Likes { get; set; }
         //public List<Comment> Comments { get; } = new();
         //add counts for likes and comments
-
-        public Blog(string author)
-        {
-            Author = author;
-        }
     }
 }
