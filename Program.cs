4d5eb1209531206564f330ff16dbd7edce702071
Chris-Costa <cdcosta807@gmail.com>
2022-12-01 13:16:58 -0500

    update cors again to include all methods

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy  =>
                       {
-                          policy.WithOrigins("http://localhost:4200").SetIsOriginAllowedToAllowWildcardSubdomains();
+                          policy.WithOrigins("http://localhost:4200", "https://localhost:7018").AllowAnyMethod().AllowAnyHeader();
                       });
 });
 // Add services to the container.
